d3975126ec7632944bc8806ce7dd89ceca64469e
z0224 <124902590+z0224@users.noreply.github.com>
2026-03-11 17:49:29 +1000

    update

--- todo/__init__.py
+++ todo/__init__.py
@@ -18,10 +18,6 @@ def create_app(config_overrides=None):
    with app.app_context(): 
       db.create_all() 
       db.session.commit() 
-
-   @app.route("/health")
-   def health():
-       return jsonify({"status": "ok"}), 200
    
    # Register the blueprints 
    from todo.views.routes import api 
